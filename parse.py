de3e393fbbf9a442606c6a7a0de3b95c15abbac6
pierre-lamarche <lamarche.p@gmail.com>
2022-01-15 22:11:57 +0100

    ajuste parser

--- parse.py
+++ parse.py
@@ -4,21 +4,27 @@ Created on 30/03/2021
 @author: pierre
 """
 
-url = "https://podcloud.fr/subscriptions/60637d562f0fc600c379f6ac.rss?k=5KXuxtdByX8lmS-tP_RMHA&u=pierrelamarche"
+rss = "http://radiofrance-podcast.net/podcast09/rss_16609.xml"
 
 import feedparser as fp
 import requests
 import datetime
+from os import listdir
 import os
-import re
+#import re
 
-podcasts = fp.parse(url)
+podcasts = fp.parse(rss)
 
-chemin_dl = "/home/pierre/Musique/dl"
+chemin_dl = "/home/pierre/Musique/La Drôle D'Humeur De Guillermo Guiz"
 
-for entry in podcasts.entries:
-    r = requests.get(entry.links[1]['href'])
+for entry in podcasts.entries[:-1]:
     timestamp = datetime.datetime.strptime(entry.published, '%a, %d %b %Y %H:%M:%S %z').strftime("%Y-%m-%d")
-    auteur = re.match(r'^\w* : \d{2}:\d{2}:\d{2} - ([^-]*) -', entry.subtitle).group(1)
-    with open(chemin_dl+'/'+timestamp+' - '+entry.title.replace('/', '')+".mp3", "wb") as f:
-        f.write(r.content)
+    #auteur = re.match(r'^\w* : \d{2}:\d{2}:\d{2} - ([^-]*) -', entry.subtitle).group(1)
+    titre = entry.title.replace('/', '')
+    annee = timestamp[:4]
+    if not os.path.exists(f'{chemin_dl}/{annee}'):
+        os.mkdir(f'{chemin_dl}/{annee}')
+    if timestamp not in [f[:10] for f in listdir(f'{chemin_dl}/{annee}')]:
+        r = requests.get(entry.links[1]['href'])
+        with open(f'{chemin_dl}/{annee}/{timestamp} - {titre}.mp3', "wb") as f:
+            f.write(r.content)
