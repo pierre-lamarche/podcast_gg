0062260c681e083ae87763fcaabe82ea98e342ab
pierre-lamarche <lamarche.p@gmail.com>
2022-01-18 23:08:51 +0100

    full parametrisation

--- scrap_gg.py
+++ scrap_gg.py
@@ -16,78 +16,87 @@ import os
 import json
 import mutagen
 from mutagen.easyid3 import EasyID3
+import argparse
 
 locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
 
-with open("urls.json", 'r') as f:
-    podcasts = json.load(f)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Defines the input json file", dest='url_json',
+                        default="urls.json")
+    parser.add_argument("-o", "--output", help="Defines the name of the output folder", dest='folder',
+                        default=".")
+    args = parser.parse_args()
+    folder = args.folder
     
-podcast = podcasts[0]
-    
-url_base = "https://www.franceinter.fr/emissions/"+podcast['url']
-browser = webdriver.Firefox()
-browser.get(url_base)
-
-# accept RGPD
-if browser.find_elements(By.ID, 'didomi-notice-agree-button'):
-        button_consent = browser.find_element(By.ID, 'didomi-notice-agree-button')
-        button_consent.click()
+    with open(args.url_json, 'r') as f:
+        podcasts = json.load(f)
         
-# get title
-album = browser.find_element(By.CLASS_NAME, 'cover-emission-title').text.lower().title()
-
-button_archives = browser.find_element(By.CLASS_NAME, 'season-link-container-link')
-button_archives.click()
-
-years = [element.text for element in browser.find_elements(By.CLASS_NAME, 'season-cover-list-element')]
-fail = []
-
-if not os.path.exists("/home/pierre/Musique/{album}"):
-    os.mkdir("/home/pierre/Musique/{album}")
-
-for year in years:
-    if not os.path.exists(f"/home/pierre/Musique/{album}/{year}"):
-        os.mkdir(f"/home/pierre/Musique/{album}/{year}")
-    browser.get(f"{url_base}/archives-{year}")
-            
-    # récupérer le nombre de pages
-    if browser.find_elements(By.CSS_SELECTOR, '.pager-item.last [href]'):
-        page_max = int(re.match(r'.*p=(\d+)$', browser.find_element(By.CSS_SELECTOR, '.pager-item.last [href]').get_attribute('href')).group(1))
-    else:
-        page_max = 1
-    
-    # loop over the pages
-    for page in range(1, page_max+1):
-        browser.get(f"{url_base}/archives-{year}?p={page}")
-        liste_podcasts = [element.get_attribute('href') for element in browser.find_elements(By.CLASS_NAME, 'card-text-sub')]
+    for podcast in podcasts:        
+        url_base = "https://www.franceinter.fr/emissions/"+podcast['url']
+        browser = webdriver.Firefox()
+        browser.get(url_base)
         
-        for cast in liste_podcasts:
-            browser.get(cast)
-            date = format(datetime.strptime(browser.find_element(By.CLASS_NAME, 'cover-emission-period').text, '%A %d %B %Y'), "%Y-%m-%d")
-            titre = browser.find_element(By.CLASS_NAME, 'cover-emission-actions-title').text.replace("/", "")
-            titre = re.sub(r'[\\/*?:"<>|]', "", titre)
-            url_dl = browser.find_element(By.CLASS_NAME, 'replay-button').get_attribute('data-url')
-            if url_dl is None:
-                fail += [date]
+        # accept RGPD
+        if browser.find_elements(By.ID, 'didomi-notice-agree-button'):
+                button_consent = browser.find_element(By.ID, 'didomi-notice-agree-button')
+                button_consent.click()
+                
+        # get title
+        album = browser.find_element(By.CLASS_NAME, 'cover-emission-title').text.lower().title()
+        
+        button_archives = browser.find_element(By.CLASS_NAME, 'season-link-container-link')
+        button_archives.click()
+        
+        years = [element.text for element in browser.find_elements(By.CLASS_NAME, 'season-cover-list-element')]
+        fail = []
+        
+        if not os.path.exists(f"{folder}/{album}"):
+            os.mkdir(f"{folder}/{album}")
+        
+        for year in years:
+            if not os.path.exists(f"{folder}/{album}/{year}"):
+                os.mkdir(f"{folder}/{album}/{year}")
+            browser.get(f"{url_base}/archives-{year}")
+                    
+            # récupérer le nombre de pages
+            if browser.find_elements(By.CSS_SELECTOR, '.pager-item.last [href]'):
+                page_max = int(re.match(r'.*p=(\d+)$', browser.find_element(By.CSS_SELECTOR, '.pager-item.last [href]').get_attribute('href')).group(1))
             else:
-                r = requests.get(url_dl)
-                fileName = f"/home/pierre/Musique/{album}/{year}/{date} - {titre}.mp3"
-                with open(fileName, "wb") as f:
-                    f.write(r.content)
-                # handle tags properly
-                try:
-                    meta = EasyID3(fileName)
-                except mutagen.id3.ID3NoHeaderError:
-                    meta = mutagen.File(fileName, easy=True)
-                    meta.add_tags()
-                meta['title'] = titre
-                meta['album'] = album
-                meta['artist'] = podcast['artist']
-                meta['date'] = date
-                meta['copyright'] = 'Radio France'
-                meta['genre'] = 'Podcast'
-                meta['website'] = url_dl
-                meta.save(fileName, v1=2)
-
-browser.close()
+                page_max = 1
+            
+            # loop over the pages
+            for page in range(1, page_max+1):
+                browser.get(f"{url_base}/archives-{year}?p={page}")
+                liste_podcasts = [element.get_attribute('href') for element in browser.find_elements(By.CLASS_NAME, 'card-text-sub')]
+                
+                for cast in liste_podcasts:
+                    browser.get(cast)
+                    date = format(datetime.strptime(browser.find_element(By.CLASS_NAME, 'cover-emission-period').text, '%A %d %B %Y'), "%Y-%m-%d")
+                    titre = browser.find_element(By.CLASS_NAME, 'cover-emission-actions-title').text
+                    titre_standardise = re.sub(r'[\\/*?:"<>|]', "", titre)
+                    url_dl = browser.find_element(By.CLASS_NAME, 'replay-button').get_attribute('data-url')
+                    if url_dl is None:
+                        fail += [date]
+                    else:
+                        r = requests.get(url_dl)
+                        fileName = f"{folder}/{album}/{year}/{date} - {titre_standardise}.mp3"
+                        with open(fileName, "wb") as f:
+                            f.write(r.content)
+                        # handle tags properly
+                        try:
+                            meta = EasyID3(fileName)
+                        except mutagen.id3.ID3NoHeaderError:
+                            meta = mutagen.File(fileName, easy=True)
+                            meta.add_tags()
+                        meta['title'] = titre
+                        meta['album'] = album
+                        meta['artist'] = podcast['artist']
+                        meta['date'] = date
+                        meta['copyright'] = 'Radio France'
+                        meta['genre'] = 'Podcast'
+                        meta['website'] = url_dl
+                        meta.save(fileName, v1=2)
         
+        browser.close()
+            
